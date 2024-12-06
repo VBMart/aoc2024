48c85568c382ca97e81f44d5fa66ee17cbab5711
VBMart <vladimirmart@gmail.com>
2024-12-06 15:19:33 +0300

    day6: make faster

--- day6.py
+++ day6.py
@@ -52,7 +52,7 @@ class Guard:
     map_h: int
     leave: bool
     loop: bool
-    history: dict
+    history: set
 
     def __init__(self, r, c, d):
         self.r = r
@@ -62,7 +62,7 @@ class Guard:
         self.map_w = 0
         self.world_map = []
         self.places = []
-        self.history = {}
+        self.history = set()
         self.loop = False
         self.leave = False
 
@@ -84,58 +84,35 @@ class Guard:
         self.print_arr(self.places)
 
     def make_step(self):
-        if self.leave:
+        if self.leave or self.loop:
             return
-        if self.loop:
+
+        direction_map = {
+            '^': (-1, 0, '>'),
+            '>': (0, 1, 'V'),
+            'V': (1, 0, '<'),
+            '<': (0, -1, '^'),
+        }
+
+        dr, dc, next_d = direction_map[self.d]
+        new_r, new_c = self.r + dr, self.c + dc
+
+        if new_r < 0 or new_r >= self.map_h or new_c < 0 or new_c >= self.map_w:
+            self.leave = True
             return
-        
-        if self.d == '^':
-            if self.r == 0:
-                self.leave = True
-                return
-            if self.world_map[self.r-1][self.c] != '#':
-                self.r -= 1
-                self.places[self.r][self.c] = 'X'
-            else:
-                self.d = '>'
-        elif self.d == '>':
-            if self.c == self.map_w-1:
-                self.leave = True
-                return
-            if self.world_map[self.r][self.c+1] != '#':
-                self.c += 1
-                self.places[self.r][self.c] = 'X'
-            else:
-                self.d = 'V'
-        elif self.d == 'V':
-            if self.r == self.map_h-1:
-                self.leave = True
-                return
-            if self.world_map[self.r+1][self.c] != '#':
-                self.r += 1
-                self.places[self.r][self.c] = 'X'
-            else:
-                self.d = '<'
-        elif self.d == '<':
-            if self.c == 0:
-                self.leave = True
-                return
-            if self.world_map[self.r][self.c-1] != '#':
-                self.c -= 1
-                self.places[self.r][self.c] = 'X'
-            else:
-                self.d = '^'
+
+        if self.world_map[new_r][new_c] != '#':
+            self.r, self.c = new_r, new_c
+            self.places[self.r][self.c] = 'X'
+        else:
+            self.d = next_d
 
         current_step = Step(r=self.r, c=self.c, d=self.d)
         if current_step in self.history:
             self.loop = True
-            return
-        # for step in self.history:
-        #     if step.c == current_step.c and step.r == current_step.r and step.d == current_step.d:
-        #         self.loop = True
-        #         return
-        self.history.append(current_step)
-    
+        else:
+            self.history.add(current_step)
+
     def get_places_count(self):
         cnt = 0
         for ih in range(0, self.map_h):
@@ -155,7 +132,7 @@ class Guard:
     def parse_map(self, lines):
         self.world_map = []
         self.places = []
-        self.history = []
+        self.history = set()
         self.loop = False
         self.leave = False
         self.map_h = len(lines)
@@ -196,7 +173,14 @@ def golden(in_txt):
     tasks = set(tasks)
     print(f'Tasks: {len(tasks)}')
 
+    t1 = time.time()
+    i = 0
     for task in tasks:
+        i += 1
+        if i % 1000 == 0:
+            print(f'Processed: {i} / {len(tasks)} time: {time.time() - t1}')
+        # if i >= 100:
+        #     break
         ih, iw = task
         guard = Guard(0, 0, '^')
         guard.parse_map(lines)
@@ -204,6 +188,8 @@ def golden(in_txt):
         guard.go()
         if guard.loop:
             loop_cnt += 1
+        # print(f'Time spent: {time.time() - start_time}')
+    print(f'Total time spent: {time.time() - t1}')
 
     print(loop_cnt)
 
@@ -232,9 +218,9 @@ def golden_thr(int_txt):
         return 0
 
     start_time = time.time()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         results = list(executor.map(check_loop, tasks))
-        print(results)
+        # print(results)
         print(sum(results))
     print(f'Time spent: {time.time() - start_time}')
 
@@ -245,5 +231,6 @@ if __name__ == "__main__":
     silver(in_txt)
     print('')
     print('Golden:')
-    # golden(in_txt)
+    golden(in_txt)
+    print('')
     golden_thr(in_txt)
